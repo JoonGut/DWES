63133822dec4cd142dd4a8405ac35e09c4bd2a88
Jon Gutierrez <gutijon48@gmail.com>
2025-10-13 11:55:53 +0200

    Cambios de test

--- testAutomovilismo.php
+++ testAutomovilismo.php
@@ -1,13 +1,22 @@
 <?php
 require_once 'F4.php';
+require_once 'F3.php';
+require_once 'F2.php';
+require_once 'F1.php';
+require_once 'FAcademy.php';
+
+
     $prueba = new F4("España","Jon","Español",12,"Ferrari",3);
     $prueba->otorgarPuntos(-1);
     echo $prueba->getPuntos();
+    echo $prueba->getNombrePiloto();
 
     $pruebaF3 = $prueba->subirCategoria("Gueñes", true);
     echo $pruebaF3->getNombreAcademia();
+    echo $pruebaF3->getNombrePiloto();
 
-    $pruebaF2 = $pruebaF3->subirCategoria("Red Bull", true);
+    $pruebaF2 = $pruebaF3->subirCategoria( true);
+    echo $pruebaF2->getNombrePiloto();
 
 
 ?>
