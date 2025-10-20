de7e9b50c34ff239c584f53170e1344b02da4df0
Jon Gutierrez <gutijon48@gmail.com>
2025-10-20 09:01:49 +0200

    Mostrar datos mediante un listado

--- PracticaAlumnado/formulario.php
+++ PracticaAlumnado/formulario.php
@@ -39,7 +39,8 @@
         <br>
         <input type="submit" value="Enviar">
     </form>
-    <section class="container.tabla">
+    <!--
+    <section class="container-tabla">
         <div class="titulo">Alumnos registrados <br></div>
         <div class="header">Nombre</div>
         <div class="header">Apellido</div>
@@ -47,31 +48,56 @@
         <div class="header">Curso</div>
         <div class="header">Email</div>
         <div class="header">Contraseña</div>
-        <div class="header">Acciones</div>
+        <div class="header">Acciones</div>-->
+    <ul class = "lista-alumnos">
+        <li class="header">Nombre</li>
+        <li class="header">Apellidos</li>
+        <li class="header">Fecha</li>
+        <li class="header">Curso</li>
+        <li class="header">Email</li>
+        <li class="header">Pass</li>
+        <li class="header">Acciones</li>
 
         <?php 
         $bd_form = "SELECT * FROM alumnado";
         $resultado= mysqli_query($conexion, $bd_form);
 
         while($row= mysqli_fetch_assoc($resultado)){ ?>
-        <div class="campo_tabla_id"><?php echo $row["id"]; ?></div>
-        <div class="campo_tabla"><?php echo $row["nombre"]; ?></div>
-        <div class="campo_tabla"><?php echo $row["apellidos"]; ?></div>
-        <div class="campo_tabla"><?php echo $row["fecha_nacimiento"]; ?></div>
-        <div class="campo_tabla"><?php echo $row["curso_matriculado"]; ?></div>
-        <div class="campo_tabla"><?php echo $row["email"]; ?></div>
-        <div class="campo_tabla"><?php echo $row["password"]; ?></div>
-        <div class="campo_tabla">
+        
+        <!--
+        <div class="campo_tabla_id"></*?php echo $row["id"]; ?></div>
+        <div class="campo_tabla"></*?php echo $row["nombre"]; ?></div>
+        <div class="campo_tabla"></*?php echo $row["apellidos"]; ?></div>
+        <div class="campo_tabla"></*?php echo $row["fecha_nacimiento"]; ?></div>
+        <div class="campo_tabla"></*?php echo $row["curso_matriculado"]; ?></div>
+        <div class="campo_tabla"></*?php echo $row["email"]; ?></div>
+        <div class="campo_tabla"></*?php echo $row["password"]; ?></div>-->
 
+
+        <li class = "campo_tabla_id"><?php echo $row["id"]; ?></li>
+        <li class="campo_tabla"><?php echo $row["nombre"]; ?></li>
+        <li class="campo_tabla"><?php echo $row["apellidos"]; ?></li>
+        <li class="campo_tabla"><?php echo $row["fecha_nacimiento"]; ?></li>
+        <li class="campo_tabla"><?php echo $row["curso_matriculado"]; ?></li>
+        <li class="campo_tabla"><?php echo $row["email"]; ?></li>
+        <li class="campo_tabla"><?php echo $row["password"]; ?></li>
+        <li class = "campo_tabla">
             <a href="eliminar.php?id=<?php echo $row['id']; ?>">Eliminar</a>
             <a href="cargar_modif.php?id=<?php echo $row['id'];?>">Editar</a>
-            <!--<a href="actualizar.php?id=<*?php echo $row['nombre'];?>" class="campo_tabla">Editar</a>
-            <a mehref="eliminar.php?nombre=<*?php echo $row['nombre'];?>" class="campo_tabla">Eliminar</a>-->
-        </div>
+        </li>
         <?php } mysqli_free_result($resultado); ?>
+    </ul>
+
+        <!--<div class="campo_tabla">
+
+            <a href="eliminar.php?id=</*?php echo $row['id']; ?>">Eliminar</a>
+            <a href="cargar_modif.php?id=</*?php echo $row['id'];?>">Editar</a>
+
+        </div>-->
+        <!-- ?php } mysqli_free_result($resultado); ? -->
 
         
-    </section>
+    <!--</section>-->
 
 
 </body>
